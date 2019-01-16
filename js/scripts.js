8b5cbe3c9d634fae6c7c886d16b82a7a0c299120
Olha Wysocky and Yulia Shidlovskaya <olha@>
2019-01-15 16:32:40 -0800

    add images appeared on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
   $(".cats .btn").click(function(){
     $(".cats ul").prepend("<li>Meow!</li>");
+    $(".cats ul").children("li").first().click(function(){
+      $(this).after("<img src='img/cats.jpg'>");
+    });
   });
 
   $(".dogs .btn").click(function(){
     $(".dogs ul").prepend("<li>Bark!</li>");
+    $(".dogs ul").children("li").first().click(function(){
+      $(this).after("<img src='img/dogs.jpg'>");
+    });
   });
+
 });
